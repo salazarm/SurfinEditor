3e72a927d2a25591bbf328ecd979a1cc58d18a23
mpan1218 <mpan1218@ubuntu>
2012-11-30 00:43:19 -0800

    Create DocListGUI

---
+++ TextEditor/src/client/DocListGUI.java
@@ -0,0 +1,11 @@
+package client;
+
+import javax.swing.*;
+
+public class DocListGUI extends JFrame{
+	
+	private JLabel documents;
+	private JTable docList;
+	private JButton newDocument;
+	private JTextField newDocumentName;
+}
