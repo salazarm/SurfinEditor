314d488e98a59ad7703951a2fdac25de48a497c1
mpan1218 <mpan1218@ubuntu>
2012-11-30 00:54:20 -0800

    Started with the constructor for DocListGUI, but haven't done much yet.

--- TextEditor/src/client/DocListGUI.java
+++ TextEditor/src/client/DocListGUI.java
@@ -1,11 +1,41 @@
 package client;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class DocListGUI extends JFrame{
 	
-	private JLabel documents;
+	private JLabel label;
 	private JTable docList;
 	private JButton newDocument;
 	private JTextField newDocumentName;
+	
+	public DocListGUI() {
+		label = new JLabel();
+		label.setName("Label");
+		label.setText("List of Existing Document");
+		
+		docList = new JTable();
+		docList.setName("Document List");
+		
+		newDocument = new JButton();
+		newDocument.setName("New Document");
+		newDocument.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		newDocumentName = new JTextField();
+		newDocumentName.setName("New Document Name");
+		newDocumentName.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+	}
 }
