9827e24c06f1cedea411484271057d4d9d09c85b
salazarm <salazarm@mit.edu>
2012-12-04 18:41:40 -0500

    Updated Server class and document model for server class

---
+++ TextEditor/src/server/Document.java
@@ -0,0 +1,81 @@
+package server;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Document {
+	private CopyOnWriteArrayList<PrintWriter> activeClients = new CopyOnWriteArrayList<PrintWriter>();
+	private final CopyOnWriteArrayList<Character> docModel;
+	private final ConcurrentLinkedQueue<String[]> commandsQueue = new ConcurrentLinkedQueue<String[]>();
+	private final String name;
+
+	/**
+	 * Constructor that makes document with Document Model docModel and Title title.
+	 * @param title the Title of the document
+	 * @param docModel the model for the document.
+	 */
+	public Document(String title, CopyOnWriteArrayList<Character> docModel) {
+		this.docModel = docModel;
+		this.name = title;
+	}
+
+	/**
+	 * Registers a user to be available to updates by adding the user to the ActiveUsers list.
+	 * @param socket the socket of the user to be added.
+	 */
+	public void addActiveUser(Socket socket) {
+		activeClients.add(Server.outs.get(socket));
+	}
+	
+	/**
+	 *  Updates all users of changes to the document model.
+	 */
+	public void updateActiveUsers(){
+		String currentDoc = this.toString();
+		for (PrintWriter out: activeClients){
+			out.print(currentDoc);
+		}
+	}
+
+	/**
+	 * Adds an insert command to this document's queue.
+	 * @param index the position where the character should be inserted
+	 * @param charToInsert the character to insert
+	 */
+	public void insertChar(int index, String charToInsert) {
+		assert(charToInsert.length() == 1);
+		commandsQueue.add(new String[]{"insert", ""+index, charToInsert});
+	}
+	
+	/**
+	 * Add a remove command to this document's queue
+	 * @param index the index of the character that should be removed.
+	 */
+
+	public void removeChar(int index) {
+		commandsQueue.add(new String[]{"remove", ""+index});
+	}
+	
+	/**
+	 *
+	 * @return the name of this document
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * @return String representationg of document (concats all the characters in the document model).
+	 */
+	@Override
+	public String toString(){
+		StringBuilder docAsString = new StringBuilder();
+		for(char c: docModel){
+			docAsString.append(c);
+		}
+		return docAsString.toString();
+	}
+
+}
---
+++ TextEditor/src/server/Server.java
@@ -0,0 +1,246 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+	private final CopyOnWriteArrayList<Document> docs = new CopyOnWriteArrayList<Document>();
+	private final String regex = "(NEW [.]+)|(\\d+ (DELETE|INSERT) \\d+)|(GET \\d+)|(CONNECT)";
+	private final ServerSocket serverSocket;
+	private Map<Socket, BufferedReader> ins = new ConcurrentHashMap<Socket, BufferedReader>();
+	protected static Map<Socket, PrintWriter> outs = new ConcurrentHashMap<Socket, PrintWriter>();
+	private List<Socket> sockets = new ArrayList<Socket>();
+	private static Random randomGenerator = new Random();
+
+	public Server(ServerSocket serverSocket) {
+		this.serverSocket = serverSocket;
+	}
+
+	private String getDoc(int ID, Socket socket) {
+		docs.get(ID).addActiveUser(socket);
+		return docs.get(ID).toString();
+	}
+
+	public static void main(String[] args) throws IOException {
+		final int port = 1337;
+		ServerSocket serverSocket = new ServerSocket(port);
+		Server server = new Server(serverSocket);
+		server.build();
+		server.serve();
+	}
+
+	/**
+	 * Reads the location of current existing documents on the server. Files are
+	 * saved line by line in a serverDocs.cfg. Each line has the location of the
+	 * localFile.
+	 * 
+	 * The grammar for the File is as follows: 
+	 * 
+	 * CONFIGFILE ::= LINE* 
+	 * LINE ::= TITLE LOCATION 
+	 * TITLE::= [.]+ 
+	 * LOCATION::= [.]+
+	 * 
+	 * @throws IOException
+	 */
+	private void build() throws IOException {
+		File f;
+		f = new File("serverDocs.cfg");
+		if (!f.exists()) {
+			f.createNewFile();
+		}
+		BufferedReader fileIn = new BufferedReader(new FileReader(f));
+
+		for (String line = fileIn.readLine(); line != null; line = fileIn
+				.readLine()) {
+			String[] tokens = line.split(" ");
+			documentize(tokens[0], tokens[1]);
+		}
+		fileIn.close();
+
+	}
+
+	private void documentize(String title, String location) throws IOException {
+		File f;
+		f = new File(location);
+		if (f.exists()) {
+			CopyOnWriteArrayList<Character> docModel = new CopyOnWriteArrayList<Character>();
+			BufferedReader fileIn = new BufferedReader(new FileReader(f));
+			for (String line = fileIn.readLine(); line != null; line = fileIn
+					.readLine()) {
+				for (int i = 0; i < line.length(); i++) {
+					docModel.add(line.charAt(i));
+				}
+				docModel.add('\n');
+			}
+			fileIn.close();
+			docs.add(new Document(title, docModel));
+		}
+	}
+
+	private void serve() throws IOException {
+		while (true) {
+			final Socket socket = serverSocket.accept();
+			sockets.add(socket);
+			Thread thread = null;
+			try {
+				thread = new Thread(new Runnable() {
+					public void run() {
+						try {
+							handleConnection(socket);
+						} catch (IOException e) {
+						}
+					}
+				});
+				thread.start();
+			} catch (Exception e) {
+				try {
+					if (!socket.isClosed()) {
+						socket.close();
+					}
+				} catch (IOException e1) {
+				}
+			} finally {
+				if (thread != null && !thread.isAlive()) {
+					if (socket.isClosed()) {
+						socket.close();
+						sockets.remove(socket);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Manages the Sockets, BufferedReaders, and PrintWriters. (Rather than have
+	 * individual methods handle thse themselves).
+	 * 
+	 * @param socket
+	 *            The socket the current connection is coming from
+	 * 
+	 * @throws IOException
+	 */
+	private void handleConnection(Socket socket) throws IOException {
+		if (!socket.isClosed()) {
+			if (!ins.containsKey(socket)) {
+				ins.put(socket,
+						new BufferedReader(new InputStreamReader(socket
+								.getInputStream())));
+				outs.put(socket,
+						new PrintWriter(socket.getOutputStream(), true));
+			}
+			BufferedReader in = ins.get(socket);
+			try {
+				for (String line = in.readLine(); line != null; line = in
+						.readLine()) {
+					handleRequest(line, socket);
+				}
+			} finally {
+				if (socket.isClosed()) {
+					in.close();
+					ins.remove(in);
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Grammar: COMMAND ::= NEW | INSERT | DELETE | GET 
+	 * NEW ::= "NEW" NAME 
+	 * NAME ::= [.]+ 
+	 * DELETE ::= "DELETE" ID INDEX 
+	 * INSERT ::= "INSERT" ID INDEX 
+	 * GET ::= "GET" ID 
+	 * ID ::= [0-9]+ 
+	 * INDEX ::= [0-9]+ 
+	 * CONNECT ::= "CONNECT"
+	 * 
+	 * @param command
+	 *            the command to be parsed
+	 */
+	private String handleRequest(String command, Socket socket) {
+		if (!command.matches(regex))
+			return null; // Design Decision: Ignore invalid commands
+		String[] tokens = command.split(" ");
+		if (tokens[0].equals("NEW")) {
+			makeNewDoc(tokens[1]);
+			return "";
+		} else if (tokens[0].equals("GET")) {
+			return getDoc(Integer.parseInt(tokens[1]), socket);
+		} else if (tokens[0].equals("CONNECT")) {
+			return getDocList();
+		} else if (tokens[0].equals("INSERT")) {
+			docs.get(Integer.parseInt(tokens[1])).insertChar(
+					Integer.parseInt(tokens[2]), tokens[3]);
+			return "";
+		} else if (tokens[0].equals("DELETE")) {
+			docs.get(Integer.parseInt(tokens[1])).removeChar(
+					Integer.parseInt(tokens[2]));
+			return "";
+		} else {
+			/* should not reach here */
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	/**
+	 * Makes a new file on the server
+	 * 
+	 * @param title
+	 *            the title of the document
+	 */
+	private void makeNewDoc(String title) {
+		File f = new File("%"); // Should never exist
+		while (!f.exists())
+			/* Design Decision* Only 200000 documents can have the same title. */
+			f = new File(title + randomGenerator.nextInt(200000));
+		docs.add(new Document(title, new CopyOnWriteArrayList<Character>()));
+		updateUsersDocList(); // Updates all users of the new file being added.
+	}
+
+	/**
+	 * Updates the users as to what documents are on the server. This method is
+	 * synchronized in case a user is added in the middle of iterating then he
+	 * won't be given an incomplete list (can happen for a split second).
+	 */
+	private synchronized void updateUsersDocList() {
+		String docsString = getDocList();
+		for (Socket socket : sockets) {
+			outs.get(socket).print(docsString);
+		}
+
+	}
+
+	/**
+	 * No need to synchronize because documents CANNOT be deleted (Another
+	 * design decision). Also if files are being added then it is okay that we
+	 * are in the middle of iterating over because we want that file to be sent
+	 * as well (Doesn't hurt). This sends a message of all of the documents to
+	 * the client in a string using the following Grammar: MESSAGE ::= (ID
+	 * TITLE)* ID ::= \\d+ TITLE ::= [.]+
+	 * 
+	 * @return all the documents as a string.
+	 */
+	private String getDocList() {
+		StringBuilder documentsString = new StringBuilder();
+		for (int i = 0; i < docs.size(); i++) {
+			/* Design Decision* : Files cannot have '%' in their name */
+
+			documentsString.append("ID" + "%" + docs.get(i).getName() + "%");
+		}
+		return documentsString.toString();
+	}
+}
