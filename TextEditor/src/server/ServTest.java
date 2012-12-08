a869ac4c1a34b256c3369aa8637ab49906257fe5
mpan1218 <mpan1218@ubuntu>
2012-12-08 13:13:24 -0800

    Server Test done Server regex fixed

--- TextEditor/src/server/ServTest.java
+++ TextEditor/src/server/ServTest.java
@@ -1,14 +1,15 @@
 package server;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import org.junit.Test;
 
@@ -35,6 +36,69 @@ import org.junit.Test;
  */
 
 public class ServTest {
+
+	@Test (timeout = 20000)
+	public void DiffCharTest() throws IOException, InterruptedException {
+    	
+    	try {
+        	File f = new File("serverDocs.cfg");
+        	f.delete();
+        }catch(Exception e){}
+    	
+		final Server server = new Server(new ServerSocket(1336));
+		server.build();
+    	
+        Thread serv = new Thread(new Runnable() {
+            public void run() {
+				try {
+					server.serve();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+                }
+        });
+        serv.start();
+        
+        // client connect
+        Socket s1 = new Socket("localhost", 1336);
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(
+                s1.getInputStream()));
+        PrintWriter p1 = new PrintWriter(s1.getOutputStream(), true);
+        
+        // make a new Document
+        p1.println("NEW sampleDoc");
+        // Waiting for document creation lag time.
+        Thread.sleep(10);
+        
+        // test digit
+        p1.println("INSERT 0 0 0");
+        // test lower case letter
+        p1.println("INSERT 0 1 a");
+        // test capital letter
+        p1.println("INSERT 0 2 A");
+        // test space
+        p1.println("INSERT 0 3");
+        // test punctuation
+        p1.println("INSERT 0 4 .");
+        // test other symbols
+        p1.println("INSERT 0 5 @");
+        
+        // server handling time
+        Thread.sleep(100);
+        
+        // read-only purpose client connected
+        Socket s2 = new Socket("localhost", 1336);
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(
+                s2.getInputStream()));
+        PrintWriter p2 = new PrintWriter(s2.getOutputStream(), true);
+        
+        // get the document
+        p2.println("GET 0");
+        
+        // check if expected.
+        assertEquals("0aA .@", br2.readLine());
+	}
+	
 	
 	/**
 	 * Test the basic functions of the server using the textClientDemo.
@@ -133,7 +197,6 @@ public class ServTest {
         	o2 = br2.readLine();
         while (o3 == null)
         	o3 = br3.readLine();
-
         System.out.println(o1);
         System.out.println(o2);
         System.out.println(o3);
@@ -238,7 +301,7 @@ public class ServTest {
         p1.println("INSERT 0 0 r");
         
         // waiting for the server to response to the typing.
-        Thread.sleep(1000);
+        Thread.sleep(500);
         
         // another client use to pull out the text.
         Socket s2 = new Socket("localhost", 1338);
@@ -257,6 +320,9 @@ public class ServTest {
         p1.println("NEW sampleDoc");
         Thread.sleep(100);
         
+        // create another document print all documents 
+        assertEquals("0%sampleDoc%1%sampleDoc%", br2.readLine());
+        
         // connect to the new document.
         p1.println("CONNECT");
         p1.println("GET 1");
@@ -274,16 +340,200 @@ public class ServTest {
         p1.println("INSERT 1 9 j");
         p1.println("INSERT 1 10 k");
         
-        Thread.sleep(1000);
+        Thread.sleep(500);
         
         p2.println("CONNECT");
         p2.println("GET 1");
         
-        // From the creating the document, all files are printed.
+        
+        // connected, all documents are printed.
         assertEquals("0%sampleDoc%1%sampleDoc%", br2.readLine());
-        // From the CONNECT command, all files are printed.
-        assertEquals("0%sampleDoc%1%sampleDoc%", br2.readLine());
-        // From the GET command, printed the file text.
+        // From the GET command, printed the document text.
         assertEquals("abcdefghijk", br2.readLine());
+    }
+    
+    @Test (timeout = 20000)
+    public void changeSameSpotTest() throws IOException, InterruptedException{
+    	try {
+        	File f = new File("serverDocs.cfg");
+        	f.delete();
+        }catch(Exception e){}
+    	
+		final Server server = new Server(new ServerSocket(1339));
+		server.build();
+    	
+        Thread serv = new Thread(new Runnable() {
+            public void run() {
+				try {
+					server.serve();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+                }
+        });
+        serv.start();
+    	
+    	// client 1 connect
+        Socket s1 = new Socket("localhost", 1339);
+        PrintWriter p1 = new PrintWriter(s1.getOutputStream(), true);
+        
+        // client 2 connect
+        Socket s2 = new Socket("localhost", 1339);
+        PrintWriter p2 = new PrintWriter(s2.getOutputStream(), true);
+        
+        // create document and wait for the create time lag.
+        p1.println("NEW sampleDoc");
+        Thread.sleep(100);
+        
+        p2.println("GET 0");
+        
+        // client 1 insert
+        p1.println("INSERT 0 0 a");
+        // Ensure that "INSERT 0 0 a" get to the server first.
+        Thread.sleep(10);
+        // client 2 insert
+        p2.println("INSERT 0 0 b");
+        
+        // server handle time
+        Thread.sleep(100);
+        
+        // client 3 connect
+        Socket s3 = new Socket("localhost", 1339);
+        BufferedReader br3 = new BufferedReader(new InputStreamReader(
+                s3.getInputStream()));
+        PrintWriter p3 = new PrintWriter(s3.getOutputStream(), true);
+        
+        // get the document.
+        p3.println("GET 0");
+        
+        // handle "INSERT 0 0 a" from p1 ---> "a".
+        // handle "INSERT 0 0 b" from p2 ---> "ba".
+        assertEquals("ba", br3.readLine());
+    }
+    
+    @Test (timeout = 20000)
+    public void concurrentTypingTest() throws IOException, InterruptedException{
+    	try {
+        	File f = new File("serverDocs.cfg");
+        	f.delete();
+        }catch(Exception e){}
+    	
+		final Server server = new Server(new ServerSocket(1340));
+		server.build();
+    	
+        Thread serv = new Thread(new Runnable() {
+            public void run() {
+				try {
+					server.serve();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+                }
+        });
+        serv.start();
+    	
+    	// client 1 connect
+        Socket s1 = new Socket("localhost", 1340);
+        PrintWriter p1 = new PrintWriter(s1.getOutputStream(), true);
+        
+        // client 2 connect
+        Socket s2 = new Socket("localhost", 1340);
+        PrintWriter p2 = new PrintWriter(s2.getOutputStream(), true);
+        
+        // create document and wait for the create time lag.
+        p1.println("NEW sampleDoc");
+        Thread.sleep(100);
+        
+        p2.println("GET 0");
+        
+        // client 1 insert
+        p1.println("INSERT 0 0 a");
+        // Ensure that "INSERT 0 0 a" get to the server first.
+        Thread.sleep(10);
+        // client 2 insert
+        p2.println("INSERT 0 1 b");
+        
+        // server handle time
+        Thread.sleep(100);
+        
+        // client 3 connect
+        Socket s3 = new Socket("localhost", 1340);
+        BufferedReader br3 = new BufferedReader(new InputStreamReader(
+                s3.getInputStream()));
+        PrintWriter p3 = new PrintWriter(s3.getOutputStream(), true);
+        
+        // get the document.
+        p3.println("GET 0");
+        
+        // handle "INSERT 0 0 a" from p1 ---> "a".
+        // handle "INSERT 0 1 b" from p2 ---> "ab".
+        assertEquals("ab", br3.readLine());
+    }
+    
+    @Test (timeout = 20000)
+    public void ThreePersonTest() throws IOException, InterruptedException{
+    	try {
+        	File f = new File("serverDocs.cfg");
+        	f.delete();
+        }catch(Exception e){}
+    	
+		final Server server = new Server(new ServerSocket(1341));
+		server.build();
+    	
+        Thread serv = new Thread(new Runnable() {
+            public void run() {
+				try {
+					server.serve();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+                }
+        });
+        serv.start();
+    	
+    	// client 1 connect
+        Socket s1 = new Socket("localhost", 1341);
+        PrintWriter p1 = new PrintWriter(s1.getOutputStream(), true);
+        
+        // client 2 connect
+        Socket s2 = new Socket("localhost", 1341);
+        PrintWriter p2 = new PrintWriter(s2.getOutputStream(), true);
+        
+        // client 3 connect
+        Socket s3 = new Socket("localhost", 1341);
+        PrintWriter p3 = new PrintWriter(s3.getOutputStream(), true);
+        
+        // create document and wait for the create time lag.
+        p1.println("NEW sampleDoc");
+        Thread.sleep(100);
+        
+        p2.println("GET 0");
+        p3.println("GET 0");
+        
+        // client 1 insert
+        p1.println("INSERT 0 0 a");
+        // Ensure that "INSERT 0 0 a" get to the server first.
+        Thread.sleep(10);
+        // client 2 insert
+        p2.println("INSERT 0 0 b");
+        Thread.sleep(10);
+        p3.println("INSERT 0 0 c");
+        
+        // server handle time
+        Thread.sleep(100);
+        
+        // client 3 connect
+        Socket s4 = new Socket("localhost", 1341);
+        BufferedReader br4 = new BufferedReader(new InputStreamReader(
+                s4.getInputStream()));
+        PrintWriter p4 = new PrintWriter(s4.getOutputStream(), true);
+        
+        // get the document.
+        p4.println("GET 0");
+        
+        // handle "INSERT 0 0 a" from p1 ---> "a".
+        // handle "INSERT 0 0 b" from p2 ---> "ba".
+        // handle "INSERT 0 0 c" from p. ---> "cba"
+        assertEquals("cba", br4.readLine());  
     }
 }
