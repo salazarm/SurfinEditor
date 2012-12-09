642ca67cccd0d28f40c085acb7c7d848713dd02b
salazarm <salazarm@mit.edu>
2012-12-09 15:27:00 -0500

    Reverting back

--- TextEditor/src/client/ServerDocumentListLoader.java
+++ TextEditor/src/client/ServerDocumentListLoader.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
@@ -22,7 +24,9 @@ import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
 
 /**
  * Opens the file picking window. Only 1 of these is ever made but the main
@@ -37,32 +41,102 @@ public class ServerDocumentListLoader {
 	private JLabel existingDocsLabel = new JLabel();
 	private JButton newDocumentButton = new JButton();
 	private JTextField newDocumentField = new JTextField();
-	protected DefaultListModel docsList = new DefaultListModel();
-	protected JList docList = new JList(docsList);
+	private DefaultListModel docsList = new DefaultListModel();
+	private JList docList = new JList(docsList);
 	private JScrollPane scroll = new JScrollPane(docList);
 	protected static JFrame mainFrame = new JFrame();
 	private JPanel mainPanel = new JPanel();
 	protected final BufferedReader in;
 	protected final PrintWriter out;
-	private final MessageHandler messageHandler;
 
 	ServerDocumentListLoader(Socket socket) throws IOException {
 		this.in = new BufferedReader(new InputStreamReader(
 				socket.getInputStream()));
 		this.out = new PrintWriter(socket.getOutputStream(), true);
-		this.messageHandler = new MessageHandler();
 		start();
 	}
 
 	protected void sendMessage(String docToSend) {
 		docToSend = StringAsciiConversion.toAscii(docToSend);
-		messageHandler.sendMessage(docToSend);
+		out.println(docToSend);
 	}
 
 	private void start() throws IOException {
 		out.println("CONNECT");
 		makeGUI();
-		messageHandler.start();
+
+		/**
+		 * This is the main thread that reads ALL incoming messages from the
+		 * server and takes appropriate action. This includes handling messages
+		 * to multiple open documents as well as updating the list of documents
+		 * on the server.
+		 */
+		(new SwingWorker<Void, String>() {
+			private Pattern regex = Pattern.compile("[\\d]+A");
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				while (true) {
+					String line = null;
+					while (line == null) {
+						try {
+							line = in.readLine();
+							publish(line);
+						} catch (IOException e) {
+							JOptionPane.showMessageDialog(null,
+									"Connection Lost", "Error",
+									JOptionPane.ERROR_MESSAGE);
+							System.exit(-1);
+						}
+					}
+				}
+			}
+
+			@Override
+			protected void process(List<String> lines) {
+				for (String line : lines) {
+					if (line.length() >= 2 && line.charAt(1) == '%') {
+						/* This is an update regarding documents on the server */
+						final String[] tokens = line.split("%");
+						System.out.println(line);
+						assert (tokens.length % 2 == 0);
+						synchronized (docList) {
+							synchronized (docsList) {
+								docsList.clear();
+								for (int i = 0; i + 1 < tokens.length; i += 2) {
+									docsList.addElement(tokens[i + 1]);
+								}
+							}
+						}
+					} else {
+						/* This is a document update */
+						Matcher matcher = regex.matcher(line);
+						if (matcher.find() && matcher.start() == 0) {
+							String id = line.substring(0, matcher.end() - 1);
+							System.out.println("PARSED ID: " + id);
+							if (ClientLoader.textEditorMap.containsKey(id)) {
+								TextEditor editor = ClientLoader.textEditorMap
+										.get(id);
+								JTextArea document = editor.document;
+
+								String docAsAsciiCode = line.substring(id
+										.length() + 1);
+								String docInAsciiText = StringAsciiConversion
+										.asciiToString(docAsAsciiCode);
+
+								int temp = editor.textAreaListener.caretPos;
+
+								document.setText(docInAsciiText);
+								document.setCaretPosition(temp);
+							}
+						} else {
+							System.out
+									.println("REGEX DIDN'T MATCH ON: " + line);
+						}
+					}
+				}
+			}
+		}).execute();
 	}
 
 	private void makeGUI() {
